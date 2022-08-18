ede42ff3249383efd69364176907f07566c2976c
vergeph <109822465+vergeph@users.noreply.github.com>
2022-08-17 20:28:54 -0400

    Vergel Paguibitan 2nd Commit
    Codes Cleanup

--- aws_servers/auto_scaling_policy.tf
+++ aws_servers/auto_scaling_policy.tf
@@ -1,3 +1,4 @@
+#Create Auto Scaling Policy
 resource "aws_autoscaling_policy" "cpu_policy_up" {
   name                   = "cpu_policy_up"
   scaling_adjustment     = 1
@@ -17,16 +18,3 @@ resource "aws_autoscaling_policy" "cpu_policy_down" {
 
 
 
-#resource "aws_autoscaling_policy" "scale_up" {
-#  name        = "target_tracking"
-#  policy_type = "TargetTrackingScaling"
-  #  scaling_adjustment     = 1
-#  autoscaling_group_name = aws_autoscaling_group.final.name
-#  target_tracking_configuration {
-#   predefined_metric_specification {
-#      predefined_metric_type = "ASGAverageCPUUtilization"
-#    }
-
-#    target_value = 10.0
-#  }
-#}
